6501ef34dc8304251e18b7a96666b5a452e9c554
zx3923 <seop3923@gmail.com>
2024-08-11 17:00:18 +0900

    account, log-in actions

---
+++ src/app/(home)/create-account/actions.ts
@@ -0,0 +1,110 @@
+"use server";
+
+import bcrypt from "bcrypt";
+import db from "@/app/lib/db";
+import { z } from "zod";
+import { redirect } from "next/navigation";
+import getSession from "@/app/lib/session";
+
+const emailRegex = new RegExp(/^[a-zA-Z0-9._%+-]+@zod\.com$/);
+
+const passwordRegex = new RegExp(/1/);
+
+const checkPasswords = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "문자만 가능합니다.",
+        required_error: "이름을 입력해주세요.",
+      })
+      .min(5, "5글자 이상")
+      .toLowerCase()
+      .trim(),
+    email: z
+      .string()
+      .email()
+      .toLowerCase()
+      .regex(emailRegex, "이메일은 zod.com"),
+    password: z.string().min(10, "10글자 이상").regex(passwordRegex, "1필수"),
+    confirm_password: z.string().min(10),
+  })
+  .superRefine(async ({ username }, ctx) => {
+    const user = await db.user.findUnique({
+      where: {
+        username,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (user) {
+      ctx.addIssue({
+        code: "custom",
+        message: "이미 존재하는 이름",
+        path: ["username"],
+        fatal: true,
+      });
+      return z.NEVER;
+    }
+  })
+  .superRefine(async ({ email }, ctx) => {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (user) {
+      ctx.addIssue({
+        code: "custom",
+        message: "이미 존재하는 이메일",
+        path: ["email"],
+        fatal: true,
+      });
+      return z.NEVER;
+    }
+  })
+  .refine(checkPasswords, {
+    message: "비밀번호가 일치하지 않습니다.",
+    path: ["confirm_password"],
+  });
+
+export async function createAccount(prevState: any, formData: FormData) {
+  const data = {
+    username: formData.get("username"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    confirm_password: formData.get("confirm_password"),
+  };
+  const result = await formSchema.spa(data);
+  if (!result.success) {
+    console.log(result.error.flatten());
+    return result.error.flatten();
+  } else {
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+    const session = await getSession();
+    session.id = user.id;
+    await session.save();
+    redirect("/profile");
+  }
+}
---
+++ src/app/(home)/log-in/actions.ts
@@ -0,0 +1,68 @@
+"use server";
+
+import bcrypt from "bcrypt";
+import db from "@/app/lib/db";
+import { z } from "zod";
+import getSession from "@/app/lib/session";
+import { redirect } from "next/navigation";
+
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return Boolean(user);
+};
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .email()
+    .toLowerCase()
+    .refine(checkEmailExists, "존재하지 않는 이메일"),
+  password: z.string({
+    required_error: "Password is required",
+  }),
+});
+
+export async function logIn(prevState: any, formData: FormData) {
+  const data = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+  const result = await formSchema.spa(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+    const ok = await bcrypt.compare(
+      result.data.password,
+      user!.password ?? "xxxx"
+    );
+    if (ok) {
+      const session = await getSession();
+      session.id = user!.id;
+      await session.save();
+      redirect("/profile");
+    } else {
+      return {
+        fieldErrors: {
+          password: ["Wrong password."],
+          email: [],
+        },
+      };
+    }
+  }
+}
