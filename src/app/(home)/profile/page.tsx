3c5d026439fe31114d85475de9d530de53b5a0c6
zx3923 <seop3923@gmail.com>
2024-08-11 16:58:53 +0900

    profile page

--- src/app/(home)/profile/page.tsx
+++ src/app/(home)/profile/page.tsx
@@ -1,5 +1,35 @@
-function Profile() {
-  return <div className="w-screen h-screen">profile</div>;
+import db from "@/app/lib/db";
+import getSession from "@/app/lib/session";
+import { notFound } from "next/navigation";
+
+async function getUser() {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+      },
+    });
+    if (user) {
+      return user;
+    }
+  }
+  notFound();
+}
+async function Profile() {
+  const user = await getUser();
+  return (
+    <div className="flex min-h-screen flex-col items-center  p-24">
+      <div>{user.id}</div>
+      <div>이메일 : {user.email}</div>
+      <div>아이디 : {user.username}</div>
+    </div>
+  );
 }
 
 export default Profile;
