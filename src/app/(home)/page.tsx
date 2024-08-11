5d9afbb2a92859e07d7dcc4ee205f8b91d055628
zx3923 <seop3923@gmail.com>
2024-08-11 16:58:28 +0900

    home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,31 +1,11 @@
-"use client";
-
-import { useFormState } from "react-dom";
-import { handleForm } from "../actions";
-import Btn from "../components/btn";
+import Link from "next/link";
 
 export default function Home() {
-  const [state, action] = useFormState(handleForm, null);
-  console.log(state);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <form action={action} className="flex flex-col gap-2">
-          <input type="email" name="email" />
-          {state?.fieldErrors.email?.map((error, i) => (
-            <span key={i}>{error}</span>
-          ))}
-          <input type="text" name="username" />
-          {state?.fieldErrors.username?.map((error, i) => (
-            <span key={i}>{error}</span>
-          ))}
-          <input type="password" name="password" />
-          {state?.fieldErrors.password?.map((error, i) => (
-            <span key={i}>{error}</span>
-          ))}
-          <Btn text="login" />
-          {state === undefined ? <span>성공</span> : null}
-        </form>
+      <div className="flex gap-5">
+        <Link href="/log-in">로그인</Link>
+        <Link href="/create-account">회원가입</Link>
       </div>
     </main>
   );
