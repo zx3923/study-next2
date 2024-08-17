4cead7e0ab72488c2f19c6763a09554b2f1549cd
zx3923 <seop3923@gmail.com>
2024-08-17 19:01:23 +0900

    tweets detail page

--- src/app/(home)/tweets/[id]/page.tsx
+++ src/app/(home)/tweets/[id]/page.tsx
@@ -1,5 +1,10 @@
 import db from "@/app/lib/db";
+import getSession from "@/app/lib/session";
 import { notFound } from "next/navigation";
+import { unstable_cache as nextCache, revalidateTag } from "next/cache";
+import LikeButton from "@/app/components/like-btn";
+import { Prisma } from "@prisma/client";
+import CommentInput from "@/app/components/commente-input";
 
 async function getTweet(id: number) {
   const tweet = await db.tweet.findUnique({
@@ -13,10 +18,67 @@ async function getTweet(id: number) {
           email: true,
         },
       },
+      _count: {
+        select: {
+          comments: true,
+          likes: true,
+        },
+      },
     },
   });
   return tweet;
 }
+
+async function getComment(id: number) {
+  const comment = await db.comment.findMany({
+    where: {
+      tweetId: id,
+    },
+    include: {
+      user: {
+        select: {
+          username: true,
+        },
+      },
+    },
+    orderBy: {
+      created_at: "desc",
+    },
+  });
+  return comment;
+}
+
+async function getCachedLikeStatus(tweeId: number) {
+  const session = await getSession();
+  const userId = session.id;
+  const cachedOperation = nextCache(getLikeStatus, ["like-status"], {
+    tags: [`like-status-${tweeId}`],
+  });
+  return cachedOperation(tweeId, userId!);
+}
+
+async function getLikeStatus(tweetId: number, userId: number) {
+  // const session = await getSession();
+  const isLiked = await db.like.findUnique({
+    where: {
+      id: {
+        tweetId,
+        userId,
+      },
+    },
+  });
+  const likeCount = await db.like.count({
+    where: {
+      tweetId,
+    },
+  });
+  return {
+    likeCount,
+    isLiked: Boolean(isLiked),
+  };
+}
+
+export type Comments = Prisma.PromiseReturnType<typeof getComment>;
 
 async function TweetDetail({ params }: { params: { id: string } }) {
   const id = Number(params.id);
@@ -24,12 +86,20 @@ async function TweetDetail({ params }: { params: { id: string } }) {
     return notFound();
   }
   const tweet = await getTweet(id);
+  const comments = await getComment(id);
   if (!tweet) {
     return notFound();
   }
+  const { likeCount, isLiked } = await getCachedLikeStatus(id);
   return (
-    <div>
-      {tweet?.id}. {tweet?.tweet}
+    <div className="min-h-screen p-24">
+      <div className="flex flex-col justify-center items-center gap-5">
+        <span className="flex justify-center">
+          {tweet?.id}. {tweet?.tweet}
+        </span>
+        <LikeButton isLiked={isLiked} likeCount={likeCount} tweeId={id} />
+        <CommentInput tweeId={id} comments={comments} />
+      </div>
     </div>
   );
 }
